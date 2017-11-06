f462ec64ea687e45df2564a07dc6b067af7aa89a
Dang Hai An <an@izberg-marketplace.com>
2017-11-06 21:27:52 +0100

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'imf-web',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Images Fight web server' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  css: [
+    // Load a node module directly (here it's a SASS file)
+
+    'bulma',
+    // SCSS file in the project
+    '@/assets/css/main.scss'
+  ]
+}
